e903dc3bec10fe65cca507f0f21dd16ae97530bc
Leonardo Felix <leonardoaalf077@hotmail.com>
2018-11-15 00:21:07 -0200

    renamed project

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-<h1>Integrating Django and Vue.js</h1>
-<h2><br />A simple recipe manager implementation with Django and Vue.</h2>
+<h1>Integrating Django and React.js</h1>
+<h2><br />A simple recipe manager implementation with Django and React.</h2>
 <p>&nbsp;</p>
 <li>The initial software description and architecture can be found <a href="https://drive.google.com/open?id=1hOcDWcIVbYaNhZ4ijZLJgVTqnYZlb_wN">Clicking Here</a></li>
 <p>&nbsp;</p>
--- django-server/src/recipes_proj/recipes_api/views.py
+++ django-server/src/recipes_proj/recipes_api/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
+from django.shortcuts        import render
+from rest_framework.views    import APIView
 from rest_framework.response import Response
+from rest_framework          import status
+
+from . import serializers
+
 # Create your views here.
 
 
 class HelloApiView(APIView):
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
 
@@ -16,3 +22,7 @@ class HelloApiView(APIView):
         ]
 
         return Response({"message": "Hello", "an_apiview": an_apiview})
+
+    def post(self, request):
+
+        serializer = serializers.HelloSerializer(data=request.data)
